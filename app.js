64940d78d32424b0d692b09f105dbd6f286327b8
Landon Reams <landon.reams.13@cnu.edu>
2016-09-27 16:13:20 -0400

    Added basic navbar
    Still non-functional

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 (function(){
-    var app = angular.module('stuff',[]);
-    app.controller('TerrariumController', function(){
+    var MainApp = angular.module('MainApp', []);
 
+    MainApp.directive('navbarTemplate', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'navbar-template.html',
+            controller: function(){
+                this.active = 'terraria';
+                this.isActive = function(page) {
+                    return this.active === page;
+                };
+                this.setActive = function(page) {
+                     this.active = page;
+                };
+                this.getActive = function() {
+                    return this.active;
+                };
+            },
+            controllerAs: 'navbarCtrl'
+        };
     });
+
 })();
